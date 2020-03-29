757cffa1a813f7417da8835335575718ddc5ddc4
Filipovici-Andrei <Filipovici-Andrei@users.noreply.github.com>
2020-03-29 17:59:25 +0300

    Fixed ssh command transmission

--- run_acceptance.sh
+++ run_acceptance.sh
@@ -3,39 +3,42 @@ echo "Checking for available host..."
 if test -z $HOST || ! grep -q "$HOST" <<< "$(floaty list --active)"
 then
     echo 'Getting new machine'
-    sleep 3
+    sleep 2
     beaker init -h windows2019-64a -o config/aio/options.rb
     export HOST=`beaker provision | grep 'Using available host' | awk {'print $4'} | xargs`
 
     echo 'Runinng tests on ' $HOST
-    sleep 3
+    sleep 2
 
     export BP_ROOT=/Users/andrei.filipovici/projects/beaker-puppet
     echo 'Puppet Agent SHA is ' $SHA
     export SHA=`curl --fail --silent GET --url http://builds.delivery.puppetlabs.net/passing-agent-SHAs/puppet-agent-master`
-    
-    echo 'Starting pre-suite'
-    sleep 3
-    
-    bundle exec beaker exec pre-suite --pre-suite $BP_ROOT/setup/common/000-delete-puppet-when-none.rb,$BP_ROOT/setup/aio/010_Install_Puppet_Agent.rb
-    
-    echo 'Setting Facter 4 repo on machine ' $HOST
-    sleep 3
 
-    ssh Administrator@$HOST "cmd /c puppet config set facterng true &&
-        cd /cygdrive/c/Program\ Files/Puppet\ Labs/Puppet/bin &&
-        mv facter-eng.bat facter.bat &&
-        facter_ng_version=`cmd /c facter-ng --version | tr -d '\r'`
-        cd /cygdrive/c/Program\ Files/Puppet\ Labs/Puppet/puppet/lib/ruby/gems/2.5.0/gems/facter-ng-$facter_ng_version &&
-        git init &&
-        git remote add origin https://github.com/puppetlabs/facter-ng.git &&
-        git fetch &&
-        git reset --hard origin/fix_acceptance"
+    echo 'Starting pre-suite'
+    sleep 2
+
+    bundle exec beaker exec pre-suite --pre-suite $BP_ROOT/setup/common/000-delete-puppet-when-none.rb,$BP_ROOT/setup/aio/010_Install_Puppet_Agent.rb
+
+    echo 'Setting Facter 4 repo on machine ' $HOST
+    sleep 2
+
+    ssh Administrator@$HOST << 'EOF'
+cmd /c puppet config set facterng true
+cd /cygdrive/c/Program\ Files/Puppet\ Labs/Puppet/bin
+mv facter-ng.bat facter.bat
+facter_ng_version=`cmd /c facter-ng --version | tr -d '\r'`
+cd /cygdrive/c/Program\ Files/Puppet\ Labs/Puppet/puppet/lib/ruby/gems/2.5.0/gems/facter-ng-$facter_ng_version
+git init
+git remote add origin https://github.com/puppetlabs/facter-ng.git
+git fetch
+git reset --hard origin/master
+EOF
 fi
-# ssh Administrator@$HOST "facter_ng_version=`cmd /c facter-ng --version | tr -d '\r'`
-#         cd /cygdrive/c/Program\ Files/Puppet\ Labs/Puppet/puppet/lib/ruby/gems/2.5.0/gems/facter-ng-$facter_ng_version &&
-#         git fetch &&
-#         git reset --hard origin/FACT-3434"
+
+#    ssh Administrator@$HOST << 'EOF'
+#git fetch
+#git reset --hard origin/FACT-2345
+#EOF
 echo 'Runinng tests on ' $HOST
 current_time=$(date +"%F:%T")
 log="$current_time.log"
